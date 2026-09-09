3d1e57a5b3d628f29d25d0e0c914f29e7e6ea346
JonathanQuin12 <josuacoba761@gmail.com>
2026-09-09 11:33:14 -0500

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -6,13 +6,13 @@ class Conexion
 {
     public static function conectar()
     {
-        $host = "hayabusa.proxy.rlwy.net";
-        $puerto = 40580;
-        $bd = "railway";
-        $usuario = "root";
-        $clave = "rXpqcGApkBfbpwtJrqUqHuMyotEzDAcO";
+        $host = getenv('DB_HOST') ?: "mysql-3ddca2b7-josuacoba761-6875.l.aivencloud.com";
+        $puerto = getenv('DB_PORT') ?: 23014;
+        $bd = getenv('DB_NAME') ?: "defaultdb";
+        $usuario = getenv('DB_USER') ?: "avnadmin";
+        $clave = getenv('DB_PASS') ?: "TU_CONTRASEÑA_DE_AIVEN";
 
-        $conn = new mysqli($host, $usuario, $clave, $bd, $puerto);
+        $conn = new mysqli($host, $usuario, $clave, $bd, (int)$puerto);
 
         if ($conn->connect_error) {
             die("Error de conexion: " . $conn->connect_error);
